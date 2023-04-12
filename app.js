4c3697cc2d06bf3435e1cfccc1687e0f407ab60f
Basharirani23 <super_boshbosh@hotmail.com>
2023-04-12 13:02:34 +0300

    Js.app now is following the requiremens

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
 let name = prompt("Please enter your name:");
-alert("Hello, " + name + "! Welcome!");
 let gender =prompt("Please enter your gender");
 gender = gender.toLowerCase();
-if (gender === "male" || gender === "female") {
-   
-    alert("Your gender is: " + gender);
-  } else {
-   
-    alert("Invalid input. Please enter either 'male' or 'female'.");
-  }
+let age=prompt("Please enter your age");
+  if (age<=0) {
+alert("Enter your real age");}
 
-
-  let age=prompt("Please enter your age");
-  if (age>0) {
-alert("Your age is " + age);
+var skipWelcome = confirm("Do you want to skip the welcoming message?");
 
 
 
-  }
-  else {alert("Your age is less than or equal 0")}
-
-  var skipWelcome = confirm("Do you want to skip the welcoming message?");
 
 if (skipWelcome) {
-  alert("Welcome message skipped!");
+  console.log("Welcome message has been skipped");
 } else {
-    var userName = prompt("Please enter your name:");
       var welcomeMessage = "";
       if (gender === "male") {
-        welcomeMessage = "Welcome, Mr. " + userName + "!";
+        welcomeMessage = "Welcome, Mr. " + name + "!";
       } else if (gender === "female") {
-        welcomeMessage = "Welcome, Ms. " + userName + "!";
-      } else {
-        welcomeMessage = "Welcome, " + userName + "!";
+        welcomeMessage = "Welcome, Ms. " + name + "!";
       }
+        
       alert (welcomeMessage)
-}
+      }
+      
+
 
 
 
